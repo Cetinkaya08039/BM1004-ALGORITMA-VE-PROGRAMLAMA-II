79a6011207c9f7d31edd073c6016a3433e57229b
Cetinkaya08039 <97054858+Cetinkaya08039@users.noreply.github.com>
2026-03-25 20:48:00 +0300

    Create Alistirma5Cevap.cs

---
+++ Alıştırmalar/Hafta-5/Alistirma5Cevap.cs
@@ -0,0 +1,54 @@
+namespace Alistirma5Cevap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Kullanıcının girdiği List<int> içinde en küçük ve en büyük sayıyı bulun.
+
+
+                İlk sorudaki liste alma yapısı korunup ı.
+            */
+
+            List<int> liste = new List<int>();
+            int toplam = 0;
+            while (true)
+            {
+                Console.Write("Listeye eklenecek bir sayı giriniz (0 = Çıkış): ");
+                if (Int32.TryParse(Console.ReadLine(), out int sayi))
+                {
+                    if (sayi == 0)
+                    {
+                        Console.WriteLine("İşlem sonlandırıldı! Oluşturulan liste ekrana yazdırılıyor...");
+                        break;
+                    }
+                    toplam += sayi;
+                    liste.Add(sayi);
+                }
+                else
+                {
+                    Console.WriteLine("Hatalı giriş yapıldı! Lütfen sayı giriniz.");
+                }
+                
+            }
+            Console.Write("Liste: ");
+            int enbuyuk = liste[0];
+            int enkucuk = liste[0];
+            foreach (int a in liste)
+            {
+                if (a > enbuyuk)
+                {
+                    enbuyuk = a;
+                }
+                if (a < enkucuk)
+                {
+                    enkucuk = a;
+                }
+                Console.Write(a + " ");
+            }
+            Console.WriteLine($"\nListedeki en büyük eleman: {enbuyuk}");
+            Console.WriteLine($"Listedeki en küçük eleman: {enkucuk}");
+        }
+    }
+}
